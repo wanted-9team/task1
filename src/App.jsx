3bb09397d4e825a52b3a633578f0005d63995688
Jaehyeon <jaehyeonp74@gmail.com>
2022-08-31 23:11:59 +0900

    [ADD] global style 적용

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import { Route, Routes, BrowserRouter } from 'react-router-dom'
 import Auth from './pages/Auth'
 import Todo from './pages/Todo'
+import GlobalStyle from './styles/global'
 
 function App() {
   return (
     <BrowserRouter>
+      <GlobalStyle/>
       <Routes>
         <Route index element={<Auth />} />
         <Route path="/todo" element={<Todo />} />
