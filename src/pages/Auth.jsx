548876c3c83b50522f0cc0ebd89a3447134bb212
jungdeokwoo <dogsocks@dogsocksui-MacBookPro.local>
2022-09-01 01:24:15 +0900

    [FIX]localstorage Token에 따른 리다이렉팅 수정

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,9 +1,14 @@
 import React, { useState } from 'react'
 import SignUp from '../components/signUp/SignUp'
 import SignIn from '../components/signIn/SignIn'
+import { Navigate } from 'react-router-dom'
 
 const Auth = () => {
   const [isLogin, setIsLogin] = useState(true)
+
+  if (localStorage.getItem('accessToken')) {
+    return <Navigate replace to={'/todo'} />
+  }
   return isLogin ? <SignIn setIsLogin={setIsLogin} /> : <SignUp setIsLogin={setIsLogin} />
 }
 export default Auth
