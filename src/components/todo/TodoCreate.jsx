b1087acc7fb72a05814a5bdfa61bc49384ba874b
[SW2/서울/2반] <dayoon1124@naver.com>
2022-09-01 02:02:43 +0900

    [ADD] todolist get 중간 commit

---
+++ src/components/todo/TodoCreate.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react'
+import { MdAdd } from 'react-icons/md'
+import { createTodo } from '../../utils/todo'
+import { CircleButton, InsertFormPositioner, InsertForm, Input } from '../../styles/Todo.style'
+import { getTodos } from '../../utils/todo'
+
+function TodoCreate({ todos }) {
+  const [open, setOpen] = useState(false)
+  const [value, setValue] = useState('')
+
+  // Add Todo
+  const addTodo = todo => {
+    createTodo(todo).then(res => {
+      if (res.status === 201) {
+        getTodos()
+      }
+    })
+  }
+
+  const onToggle = () => setOpen(!open)
+  const onChange = e => setValue(e.target.value)
+  const onSubmit = e => {
+    e.preventDefault()
+    addTodo({ todo: value })
+    setOpen(false)
+    setValue('')
+  }
+  return (
+    <>
+      {open && (
+        <InsertFormPositioner>
+          <InsertForm onSubmit={onSubmit}>
+            <Input
+              autoFocus
+              onChange={onChange}
+              value={value}
+              placeholder="할 일을 입력 후, enter를 누르세요"
+            />
+          </InsertForm>
+        </InsertFormPositioner>
+      )}
+      <CircleButton onClick={onToggle} open={open}>
+        <MdAdd />
+      </CircleButton>
+    </>
+  )
+}
+
+export default TodoCreate
